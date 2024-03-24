b58ed99f83afede6ec7419082d3d5b6d57e734d3
Sagacode <ishimwe.sage.3@gmail.com>
2024-03-24 22:00:38 +0200

    Update TodoModel to use Lombok Setterd

--- src/main/java/com/sage/java/RestfulApi/Models/TodoModel.java
+++ src/main/java/com/sage/java/RestfulApi/Models/TodoModel.java
@@ -3,7 +3,9 @@ package com.sage.java.RestfulApi.Models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 @Entity
 @Table(name = "todos")
@@ -20,15 +22,4 @@ public class TodoModel {
     private String description;
 
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
