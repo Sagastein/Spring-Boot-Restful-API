e31cd6b5d73e20f308f56a2ef4b6dcd3233e9d79
Sagacode <ishimwe.sage.3@gmail.com>
2024-03-28 20:34:33 +0200

    Add a Reusable response handler

---
+++ src/main/java/com/sage/java/RestfulApi/Service/ResponseUtils.java
@@ -0,0 +1,16 @@
+package com.sage.java.RestfulApi.Service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseUtils {
+
+    public static ResponseEntity<?> createErrorResponse(String message, HttpStatus status) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", message);
+        return new ResponseEntity<>(error, status);
+    }
+}
